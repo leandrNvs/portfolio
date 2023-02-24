ca859f35eff0c419a642e3d8230c0f09ee88b33e
Leandro Neves <leandro_-_neves@hotmail.com>
2023-02-24 17:11:10 -0300

    Link para o repositorio do portfolio adicionado

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,11 @@ export default function Footer() {
       <F.Text>
         Criado por{" "}
         <F.Link href="https://github.com/leandrNvs" target="_blank">
-          Leandro Neves
+          Leandro Neves.
+        </F.Link>{" "}
+        Dê uma olhada no{" "}
+        <F.Link href="https://github.com/leandrNvs/portfolio" target="_blank">
+          código.
         </F.Link>
       </F.Text>
     </F.Footer>
