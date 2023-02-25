55540b453df7cead3f482714e5fa2cbb7cd61dd8
Leandro Neves <leandro_-_neves@hotmail.com>
2023-02-25 14:44:10 -0300

    Imagens dos projetos adicionados a pasta public/

--- src/data/data.ts
+++ src/data/data.ts
@@ -6,181 +6,157 @@ export default [
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/crowdfunding-product-page/raw/main/desktop.png",
+    thumb: "/images/crowdfunding-product-page.png",
     code: "https://github.com/leandrNvs/crowdfunding-product-page",
     live: "https://crowdfunding-landingpage-frontend.netlify.app/",
     technologies: ["vite", "reactjs", "sass"],
   },
   {
-    thumb: "https://github.com/leandrNvs/app-financas/raw/main/desktop.png",
+    thumb: "/images/app-financas.png",
     code: "https://github.com/leandrNvs/app-financas",
     live: "https://app-financas-react.netlify.app/",
     technologies: ["vite", "reactJs", "styled-components"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/time-tracking-dashboard/raw/main/desktop.png",
+    thumb: "/images/time-tracking-dashboard.png",
     code: "https://github.com/leandrNvs/time-tracking-dashboard",
     live: "https://time-tracking-dashboard-frontend.netlify.app/",
     technologies: ["vite", "reactjs", "sass"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Pricing-component-with-toggle/raw/main/desktop.png",
+    thumb: "/images/Pricing-component-with-toggle.png",
     code: "https://github.com/leandrNvs/Pricing-component-with-toggle",
     live: "https://pricing-component-with-toggle-front.netlify.app/",
     technologies: ["vite", "reactjs", "styled-components"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Social-proof-section/raw/main/desktop.png",
+    thumb: "/images/Social-proof-section.png",
     code: "https://github.com/leandrNvs/Social-proof-section",
     live: "https://leandrnvs.github.io/Social-proof-section/",
     technologies: ["html", "sass"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Intro-component-with-sign-up-form/raw/main/desktop.png",
+    thumb: "/images/Intro-component-with-sign-up-form.png",
     code: "https://github.com/leandrNvs/Intro-component-with-sign-up-form",
     live: "https://leandrnvs.github.io/Intro-component-with-sign-up-form/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Fylo-data-storage-component/raw/main/desktop.png",
+    thumb: "/images/Fylo-data-storage-component.png",
     code: "https://github.com/leandrNvs/Fylo-data-storage-component",
     live: "https://leandrnvs.github.io/Fylo-data-storage-component/",
     technologies: ["html", "css"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Clipboard-landing-page/raw/main/desktop.png",
+    thumb: "/images/Clipboard-landing-page.png",
     code: "https://github.com/leandrNvs/Clipboard-landing-page",
     live: "https://leandrnvs.github.io/Clipboard-landing-page/",
     technologies: ["html", "css"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Huddle-landing-page-with-curved-sections/raw/main/desktop.png",
+    thumb: "/images/Huddle-landing-page-with-curved-sections.png",
     code: "https://github.com/leandrNvs/Huddle-landing-page-with-curved-sections",
     live: "https://leandrnvs.github.io/Huddle-landing-page-with-curved-sections/",
     technologies: ["html", "css"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Insure-landing-page/raw/main/desktop.png",
+    thumb: "/images/Insure-landing-page.png",
     code: "https://github.com/leandrNvs/Insure-landing-page",
     live: "https://leandrnvs.github.io/Insure-landing-page/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Project-tracking-intro-component/raw/main/desktop.png",
+    thumb: "/images/Project-tracking-intro-component.png",
     code: "https://github.com/leandrNvs/Project-tracking-intro-component",
     live: "https://leandrnvs.github.io/Project-tracking-intro-component/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Loopstudios-landing-page/raw/main/desktop.png",
+    thumb: "/images/Loopstudios-landing-page.png",
     code: "https://github.com/leandrNvs/Loopstudios-landing-page",
     live: "https://leandrnvs.github.io/Loopstudios-landing-page/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Fylo-dark-theme-landing-page/raw/main/desktop.png",
+    thumb: "/images/Fylo-dark-theme-landing-page.png",
     code: "https://github.com/leandrNvs/Fylo-dark-theme-landing-page",
     live: "https://leandrnvs.github.io/Fylo-dark-theme-landing-page/",
     technologies: ["html", "sass", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/FAQ-accordion-card/raw/main/desktop.png",
+    thumb: "/images/FAQ-accordion-card.png",
     code: "https://github.com/leandrNvs/FAQ-accordion-card",
     live: "https://leandrnvs.github.io/FAQ-accordion-card/",
     technologies: ["html", "sass", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Notifications-page/raw/main/desktop.png",
+    thumb: "/images/Notifications-page.png",
     code: "https://github.com/leandrNvs/Notifications-page",
     live: "https://leandrnvs.github.io/Notifications-page/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Intro-section-with-dropdown-navigation/raw/main/desktop.png",
+    thumb: "/images/Intro-section-with-dropdown-navigation.png",
     code: "https://github.com/leandrNvs/Intro-section-with-dropdown-navigation",
     live: "https://leandrnvs.github.io/Intro-section-with-dropdown-navigation/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/blogr-landing-page/raw/main/desktop.png",
+    thumb: "/images/blogr-landing-page.png",
     code: "https://github.com/leandrNvs/blogr-landing-page",
     live: "https://leandrnvs.github.io/blogr-landing-page/",
     technologies: ["html", "sass", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Coding-bootcamp-testimonials-slider/raw/main/desktop.png",
+    thumb: "/images/Coding-bootcamp-testimonials-slider.png",
     code: "https://github.com/leandrNvs/Coding-bootcamp-testimonials-slider",
     live: "https://leandrnvs.github.io/Coding-bootcamp-testimonials-slider/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Fylo-landing-page-with-two-column-layout/raw/main/desktop.png",
+    thumb: "/images/Fylo-landing-page-with-two-column-layout.png",
     code: "https://github.com/leandrNvs/Fylo-landing-page-with-two-column-layout",
     live: "https://leandrnvs.github.io/Fylo-landing-page-with-two-column-layout/",
     technologies: ["html", "css"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Tip-calculator-app/raw/main/desktop.png",
+    thumb: "/images/Tip-calculator-app.png",
     code: "https://github.com/leandrNvs/Tip-calculator-app",
     live: "https://leandrnvs.github.io/Tip-calculator-app/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb: "https://github.com/leandrNvs/todo-list/raw/main/desktop.png",
+    thumb: "/images/todo-list.png",
     code: "https://github.com/leandrNvs/todo-list",
     live: "https://todo-list-typescriptreact.netlify.app/",
     technologies: ["vite", "reactjs", "typescript", "styled-components"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/NFT-preview-card-component/raw/main/screenshot.png",
+    thumb: "/images/NFT-preview-card-component.png",
     code: "https://github.com/leandrNvs/NFT-preview-card-component",
     live: "https://leandrnvs.github.io/NFT-preview-card-component/",
     technologies: ["html", "css"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Interactive-pricing-component/raw/main/desktop.png",
+    thumb: "/images/Interactive-pricing-component.png",
     code: "https://github.com/leandrNvs/Interactive-pricing-component",
     live: "https://leandrnvs.github.io/Interactive-pricing-component/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/digital-agency-bootstrap/raw/main/desktop.png",
+    thumb: "/images/digital-agency-bootstrap.png",
     code: "https://github.com/leandrNvs/digital-agency-bootstrap",
     live: "https://leandrnvs.github.io/digital-agency-bootstrap/",
     technologies: ["html", "bootstrap"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Social-media-dashboard-with-theme-switcher/raw/main/desktop-light.png",
+    thumb: "/images/Social-media-dashboard-with-theme-switcher.png",
     code: "https://github.com/leandrNvs/Social-media-dashboard-with-theme-switcher",
     live: "https://leandrnvs.github.io/Social-media-dashboard-with-theme-switcher/",
     technologies: ["html", "css", "javascript"],
   },
   {
-    thumb:
-      "https://github.com/leandrNvs/Expenses-chart-component/raw/main/screenshot.png",
+    thumb: "/images/Expenses-chart-component.png",
     code: "https://github.com/leandrNvs/Expenses-chart-component",
     live: "https://leandrnvs.github.io/Expenses-chart-component/",
     technologies: ["html", "css", "javascript"],
